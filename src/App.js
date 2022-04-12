deef1f2bb701f6814d97a625041457c7763b7d23
Ninroot-Eater <jamesthiha@Jamess-MacBook-Pro.local>
2022-04-12 20:28:21 +0800

    meta tags added to index.html directly

--- src/App.js
+++ src/App.js
@@ -12,15 +12,7 @@ function App() {
     <div>
       <Helmet>
         <title>Thiha Swan Htet | Home</title>
-        <meta charSet="utf-8"/>
-        <meta name="keywords" content='Thiha, portfolio, Python, Django, backend'/>
-        <meta name="description" content='Hi! I am Thiha, and this is my portfolio website.'/>
-        <meta name='author' content='Thiha Swan Htet'/>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
-
-        <meta property="og:title" content="Thiha Swan Htet | Portfolio" />
-        <meta property="og:url" content="http://www.thiha.net" />
-        <meta property="og:image" content="/images/other/face.jpeg" />        
+             
       </Helmet>
         <div className="bigdiv">
           <div className="big-container">
